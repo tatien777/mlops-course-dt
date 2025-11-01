afbeec6aac87e64904482a553ec1cf80da1b640f
Your Name <your.email@example.com>
2025-11-01 17:22:08 +1030

    Seesion 3

---
+++ scripts/session3/api.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+import mlflow.sklearn
+import pandas as pd 
+import uvicorn 
+from pydantic import BaseModel
+from enum import Enum
+from scripts.session3.router import predict,utils
+
+
+app = FastAPI()
+app.include_router(predict.housing_router)
+app.include_router(utils.utils_router)
+
+@app.get("/")
+def read_root() -> dict[str,str]:
+    return {"message": "Hello, FastAPI!"}
+
+
+
+
+class Method(str,Enum):
+        add = "add"
+        subtract = "subtract"
+        multiply = "multiply"
+        divide = "divide"
+
+class CalculateRequest(BaseModel):
+    method: Method
+    num1: float
+    num2: float
+
+class CalculateResponse(BaseModel):
+    result: float
+
+@app.post("/calculate",response_model=CalculateResponse)
+def calculate(request: CalculateRequest) -> CalculateResponse:
+    if request.method == Method.add:
+        result = float(request.num1 + request.num2)
+    elif request.method == Method.subtract:
+        result = float(request.num1 - request.num2)
+    elif request.method == Method.multiply:
+        result = float(request.num1 * request.num2)
+    elif request.method == Method.divide:
+        result = float(request.num1 / request.num2)
+    else:
+        raise ValueError(f"Invalid method: {request.method}")
+    return CalculateResponse(result=result)
+
+if __name__ == '__main__':
+    uvicorn.run("api:app",host="0.0.0.0",port=8000,reload=True)
